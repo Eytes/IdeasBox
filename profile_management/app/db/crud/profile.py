b4627beb0d4e872336ebeaceb80f215e46778d67
Eytes <yura.shamanov2000@bk.ru>
2023-12-26 21:28:18 +0200

    first commit

---
+++ profile_management/app/db/crud/profile.py
@@ -0,0 +1,11 @@
+from app.db.crud._common import (
+    create_document,
+)
+
+
+def create_user():
+    # TODO: создать модель пользователя
+    # TODO: проверить существование пользователя
+    # TODO: создать исключение UserExistsError
+    # TODO: если пользователя не существует, то создать, иначе вызвать исключение UserExistsError
+    pass
